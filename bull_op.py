891fcc4d4455d830b83123a54d492e2a142d185f
Carlos <carlosdj0305@gmail.com>
2025-01-17 13:21:06 +0100

    added 2 more files

---
+++ bull_op.py
@@ -0,0 +1,10 @@
+print(not 7)
+print(not 0)
+
+print(1 or 2 or 3)
+print(1 and 2 and 3)
+
+print(0 or Lase or 11 or None)
+print(1 and 2 and False and 9)
+
+#The one that breaks the chain comes out or the last one
